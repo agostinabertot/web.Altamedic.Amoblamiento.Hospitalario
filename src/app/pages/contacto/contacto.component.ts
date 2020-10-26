d68f3f51893b83de4fa0d6aea8a0b4ed0ecf1a07
Martin Cardoso <mcardoso821@gmail.com>
2020-10-25 22:09:12 -0300

    added netlify service

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NetlifyFormsService } from 'src/app/netlify-forms/netlify-forms.service';
 
 @Component({
   selector: 'app-contacto',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  contactForm = this.fb.group({
+    nombre: ['', Validators.required],
+    email: ['', [Validators.email, Validators.required]],
+    asunto: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private netlifyForms: NetlifyFormsService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.netlifyForms.submitContact(this.contactForm.value).subscribe(
+      () => {
+        this.contactForm.reset();
+        this.router.navigateByUrl('/success');
+      }
+    );
   }
 
 }
